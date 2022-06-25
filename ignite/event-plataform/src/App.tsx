5d7ef103fb674fa5663920bd3daa70c15f8321d9
VitorZboniq <88394895+vitorzboniq@users.noreply.github.com>
2022-06-25 19:50:24 -0300

    Aula02 Finalizada
    Aula02 Finalizada

--- ignite/event-plataform/src/App.tsx
+++ ignite/event-plataform/src/App.tsx
@@ -1,6 +1,11 @@
 import{ gql, useQuery} from "@apollo/client"
 import { useEffect } from "react"
+import { Header } from "./components/Header";
+import { Lesson } from "./components/Lesson";
+import { Sidebar } from "./components/Sidebar";
+import { Video } from "./components/Video";
 import { client } from "./lib/apollo"
+import { Event } from "./pages/Event";
 
 const GET_LESSONS_QUERY = gql `
   query {
@@ -20,11 +25,15 @@ function App() {
   const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
 
   return (
-    <ul>
-      {data?.lessons.map(lesson => {
-        return <li key={lesson.id}>{lesson.title}</li>
-      })}
-    </ul>
+    // <ul>
+    //   {data?.lessons.map(lesson => {
+    //     return <li key={lesson.id}>{lesson.title}</li>
+    //   })}
+    // </ul>
+
+    <div>
+        <Event/>
+    </div>
   )
 }
 
