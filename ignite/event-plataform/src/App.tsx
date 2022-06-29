e9cda85372d74d98aaac7143797ff544628bfe1e
VitorZboniq <88394895+vitorzboniq@users.noreply.github.com>
2022-06-29 20:24:04 -0300

    ok
    ok

--- ignite/event-plataform/src/App.tsx
+++ ignite/event-plataform/src/App.tsx
@@ -1,39 +1,33 @@
-import{ gql, useQuery} from "@apollo/client"
-import { useEffect } from "react"
-import { Header } from "./components/Header";
+import{ ApolloProvider, gql, useQuery} from "@apollo/client"
+import { BrowserRouter } from "react-router-dom";
 import { Lesson } from "./components/Lesson";
-import { Sidebar } from "./components/Sidebar";
-import { Video } from "./components/Video";
 import { client } from "./lib/apollo"
 import { Event } from "./pages/Event";
+import { Router } from "./router";
 
-const GET_LESSONS_QUERY = gql `
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
+// const GET_LESSONS_QUERY = gql `
+//   query {
+//     lessons {
+//       id
+//       title
+//     }
+//   }
+// `
 
-interface Lesson {
-  id: string;
-  title: string;
-}
+// interface Lesson {
+//   id: string;
+//   title: string;
+// }
 
 function App() {
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
+  // const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
 
   return (
-    // <ul>
-    //   {data?.lessons.map(lesson => {
-    //     return <li key={lesson.id}>{lesson.title}</li>
-    //   })}
-    // </ul>
-
-    <div>
-        <Event/>
-    </div>
+        <ApolloProvider client={client}>
+          <BrowserRouter>
+            <Router/>
+          </BrowserRouter>
+        </ApolloProvider>
   )
 }
 
